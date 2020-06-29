2b5bfb2b5ac13e8ac193bbf5644a53b5b5634365
Tilier <61211693+Tilier@users.noreply.github.com>
2020-06-29 18:28:05 -0400

    Initial upload

---
+++ indexSimple.js
@@ -0,0 +1,35 @@
+var search = document.querySelector('#search');
+document.documentElement.style.setProperty('--height', window.innerHeight);
+
+scrollfunc();
+window.addEventListener('scroll', scrollfunc);
+window.addEventListener('load', loaded);
+
+$.getJSON("https://api.scratch.mit.edu/proxy/featured", function(data) {
+    w3.displayObject("curator", data);
+        });
+
+search.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+        window.location.href = '/search/projects.html?q=' + search.value;
+    }
+});
+
+function scrollfunc() {
+    if (window.pageYOffset || window.scrollTop == '1') {
+        document.querySelector('nav').style.boxShadow = '0 0 20px rgba(0,0,0,0.11)'
+    } else {
+        document.querySelector('nav').style.boxShadow = 'none'
+    }
+}
+
+function loaded() {
+    var loader = document.querySelector('.loader');
+    window.setTimeout(function(){
+        loader.classList.add('done');
+        document.body.style.overflowY = 'auto';}
+        
+    , 500);
+}
+
+console.log(window.location.pathname.split("/").filter(a => a.length > 0).length);
---
+++ projectsJSON.js
@@ -0,0 +1,20 @@
+$.getJSON("https://cors-anywhere.herokuapp.com/https://api.scratch.mit.edu/proxy/featured/", function(udata) {
+    var data = udata.curator_top_projects;
+    console.log(data);
+    var itemone = data[1];
+    var curator = itemone.curator_name;
+    document.querySelector('#curator').innerHTML = curator;
+    for (i = 0; i < 5; i++) {
+        var curates = document.querySelector('.cgrid');
+        var entry = data[i];
+        var element = `
+        <div class="item curated">
+            <img src="https://uploads.scratch.mit.edu/projects/thumbnails/` + entry.id +  `.png" class="projectc">
+            <h3>` + entry.title +  `</h3>
+            <h6>` + entry.creator +  `</h6>
+        </div>
+    `;
+        $(curates).append(element);
+        console.log('hi');
+    };
+});
