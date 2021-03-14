34377b9a5e276e0e1debc9c6179a19401a40f4e0
Tilier <61211693+Tilier@users.noreply.github.com>
2021-03-14 18:38:23 -0400

    ...

--- userPage.js
+++ userPage.js
@@ -47,8 +47,9 @@ document.body.innerHTML = `
         function getUserData(username) {
             $.getJSON("https://cors.bridged.cc/https://api.scratch.mit.edu/users/" + username, function(gudata) {
                     const regex = /@([\w\s\d_-]*)(?:[^\.'<\bbr\b/*>].[^\s])*/g;
+                   
                     
-                document.querySelector("#about").innerHTML = gudata.profile.bio.replace(/\n/g, "<br>").replaceAll(regex, '<a href="example.com/users/$1">$&</a>');
-                document.querySelector("#working").innerHTML = gudata.profile.status.replace(/\n/g, "<br>").replaceAll(regex, '<a href="example.com/users/$1">$&</a>');
+                document.querySelector("#about").innerHTML = gudata.profile.bio.replace(/\n/g, "<br>").replaceAll(regex, '<a href="https://scratchredesign.ml/users/$1">$&</a>');
+                document.querySelector("#working").innerHTML = gudata.profile.status.replace(/\n/g, "<br>").replaceAll(regex, '<a href="https://scratchredesign.ml/users/$1">$&</a>');
             });
         }
